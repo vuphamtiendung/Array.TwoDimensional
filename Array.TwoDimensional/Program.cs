179014e3fde87160cfe217790534d49ec4095674
DungVPT <vuphamtiendung@gmail.com>
2023-12-22 21:07:32 +0700

    Git Commit version 0.2

--- Array.TwoDimensional/Program.cs
+++ Array.TwoDimensional/Program.cs
@@ -4,30 +4,28 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        WriteLine("Input amount of the first array:");
+        WriteLine("Input amount in the array:");
         int amountFirstArray = Convert.ToInt32(ReadLine());
-        WriteLine("Input amount of the second array:");
+        WriteLine("Input amount in the array:");
         int amountSecondArray = Convert.ToInt32(ReadLine());
-        int[,] array = new int[amountFirstArray, amountSecondArray];
-
-        int lengthFirstArray = array.GetLength(0); // get length of the row
-        int lengthSecondArray = array.GetLength(1); // get length of the column
-
-        WriteLine($"Length of the first array: {lengthFirstArray}");
-        WriteLine($"Length of the second array: {lengthSecondArray}");
 
         float totalFirstGroup = 0.0f;
         float totalSecondGroup = 0.0f;
 
+        int[,] array = new int[amountFirstArray, amountSecondArray];
+
+        int lengthFirstArray = array.GetLength(0); // length of the fisrt array
+        int lengthSecondArray = array.GetLength(1); // length of the second array
+
         WriteLine("Input data in the array:");
         for(int firstIndex = 0; firstIndex < lengthFirstArray; firstIndex++)
         {
-            for(int secondIndex = 0;  secondIndex < lengthSecondArray; secondIndex++)
+            for(int secondIndex = 0; secondIndex < lengthSecondArray; secondIndex++)
             {
                 array[firstIndex, secondIndex] = Convert.ToInt32(ReadLine());
             }
         }
-        
+
         for(int index = 0; index < lengthSecondArray; index++)
         {
             totalFirstGroup += array[0, index];
@@ -37,9 +35,9 @@ internal class Program
         {
             totalSecondGroup += array[1, index];
         }
-
-        WriteLine($"total first group: {totalFirstGroup / lengthSecondArray}");
-        WriteLine($"total second group: {totalSecondGroup / lengthSecondArray}");
+        
+        WriteLine($"First Group: {totalFirstGroup / lengthSecondArray}");
+        WriteLine($"First Group: {totalSecondGroup / lengthSecondArray}");
 
         ReadLine();
     }
